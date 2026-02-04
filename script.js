fa411c70deb6e4b3e7b924e72584a87092ff676a
Flavim4004 <215928528+Flavim4004@users.noreply.github.com>
2026-02-03 21:52:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let contador = 1
+document.querySelector("#bola1").checked = true
+
+setInterval(function () {
+    proxima();
+}, 3000);
+
+function proxima() {
+    contador = contador + 1 
+    
+    if (contador > 3) {
+        contador = 1
+    }
+    document.querySelector("#bola" + contador).checked = true
+
+}
